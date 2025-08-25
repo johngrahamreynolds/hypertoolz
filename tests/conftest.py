2550c525a3c9ba19876978dcc87ee1560b14d22e
John Graham Reynolds <john.graham.reynolds@vumc.org>
2025-08-25 23:48:22 +0000

    add updated conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ import pytest
 
 @pytest.fixture(scope="session")
 def output_root() -> str:
-    return "/workspaces/hypertoolz/tests/output/"
+    return "/workspaces/hypertoolz/tests/output/"  # TODO
 
 
 @pytest.fixture(scope="session")
@@ -18,6 +18,15 @@ def test_files_root() -> str:
 @pytest.fixture(scope="session")
 def config_yaml_path(test_files_root: str) -> str:
     return os.path.join(test_files_root, "example_tuner_config.yaml")
+
+
+@pytest.fixture
+def sample_param_ranges():
+    return {
+        "learning_rate": (1e-5, 1e-2),
+        "batch_size": [32, 64, 128],
+        "n_epochs": (5, 15),
+    }
 
 
 @pytest.fixture(autouse=True, scope="function")
